7ef3fd53aef99eacaa8eb78e4bcaae53a5358028
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2023-08-20 17:50:17 +0100

    forgot password controller function part-2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -249,7 +249,28 @@ const forgotPassword = asyncHandler(async (req, res) => {
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
-  console.log(hashedToken);
+
+  //save Token to DB
+  await new Token({
+    userId: user._id,
+    token: hashedToken,
+    createAt: Date.now(),
+    expiresAt: Date.now() + 30 * (60 * 1000), // Thirty minutes
+  }).save();
+
+  //construct Reset url
+
+  const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+  //reset email
+  const message = `<h2>Hello ${user.name}</h2>
+    <p>please use the url below to reset your password</p>
+    <p>This reset lik is valid for 30 minutes</p>
+    <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+
+    <p>Regards</P>
+     <p>Ecommerce Team</P>
+    `;
   res.send("Forgot password");
 });
 
